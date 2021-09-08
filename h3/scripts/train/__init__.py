44304be6690446dc80e2e5d2ea4f1798a4c6b3fc
Hyeonki Hong <hhk7734@gmail.com>
2021-09-08 22:06:17 +0900

    scripts: train: add `--batch_size`

--- h3/scripts/train/__init__.py
+++ h3/scripts/train/__init__.py
@@ -9,6 +9,8 @@ class Arguments(ArgumentsBaseClass):
 
         self.dataset_dir: Path
         self._keys.append("dataset_dir")
+        self.batch_size: int
+        self._keys.append("batch_size")
 
     def add_arguments(self) -> None:
         super().add_arguments()
@@ -20,12 +22,20 @@ class Arguments(ArgumentsBaseClass):
             required=True,
         )
 
+        self._parser.add_argument(
+            "--batch_size",
+            default=8,
+            type=int,
+        )
+
     def validate(self) -> None:
+        # pylint: disable=useless-super-delegation
         super().validate()
 
     def __str__(self) -> str:
         string = super().__str__()
 
         string += f" --dataset_dir {self.dataset_dir}"
+        string += f" --batch_size {self.batch_size}"
 
         return string
